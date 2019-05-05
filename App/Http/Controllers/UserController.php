63af86a380cc8bb4c7e8f1ec37ba4a99774b6007
Genarodaniel <37519144+Genarodaniel@users.noreply.github.com>
2019-05-05 15:02:57 -0300

    aprendizagem inicial

---
+++ App/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<? php
+	namespace App\http\Controllers;
+
+
+	class UserController extends Controller{
+
+		public function list(){
+			return '<h1>Informações do usuário</h1>
+			<b>Nome: </b>Otario</br>';
+
+		}
+
+	}
+	return 'ok';
+
+	?>
